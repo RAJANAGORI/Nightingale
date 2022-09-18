771542d2e3c3f1fdf0d635902c749af7413d8075
MrBotCipher <32300516+RAJANAGORI@users.noreply.github.com>
2022-09-19 00:23:12 +0530

    Go module installation step change

--- configuration/modules-installation/go-install-modules.sh
+++ configuration/modules-installation/go-install-modules.sh
@@ -1,3 +1,2 @@
-go get github.com/evilsocket/xray
-cd $GOPATH/src/github.com/evilsocket/xray/ && make
-mv /root/go/src/github.com/evilsocket/xray/build /usr/local/bin/xray 
+cd ${TOOLS_OSINT}/xray && \
+    make
