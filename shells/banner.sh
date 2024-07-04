530d673e39be7302cb7708547945d226f2b30911
MrBotCipher <32300516+RAJANAGORI@users.noreply.github.com>
2024-07-04 21:34:56 +0530

    Fis: Added Hints for activate command

--- shells/banner.sh
+++ shells/banner.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+
+# Print the "Nightingale" banner
 figlet Nightingale
+
 figlet -f term "Made by Raja Nagori <3 from India"
+
+figlet -f term "Run 'activate python' and 'activate go' one time only"
+
+figlet -f term "Run 'activate --help' for more options"
+
+# Set up aliases for ping and ping6
 alias ping="ping -4"
 alias ping6="ping -6"
